879478c4d701d53d039fe0f3346b3992ead06a4a
Konstantin Tenzin <tenzink@yandex.ru>
2021-04-03 02:36:26 +0300

    Move customization to the custom.el

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(setq ring-bell-function 'ignore)
+(let
+    ((customization-file (expand-file-name "custom.el" user-emacs-directory)))
+  (when (file-exists-p customization-file)
+    (setq custom-file customization-file)
+    (load custom-file 'noerror)))
 
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
