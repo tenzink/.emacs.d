6e996f2a3833793b772b55c0f795d044bcc1c1ca
Konstantin Tenzin <tenzink@yandex.ru>
2021-04-03 03:08:26 +0300

    utf-8 everywhere

--- init.el
+++ init.el
@@ -26,3 +26,9 @@
 
 (use-package no-littering)
 (require 'no-littering)
+
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
